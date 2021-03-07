053c23e008e55a8c6cca0f18163e30ba385f5e98
Maxym Denysenko <mdenisenko95@gmail.com>
2021-03-07 23:45:31 +0200

    Add exception if tested file doesn't exist

--- WPForms/Tests/TestCase.php
+++ WPForms/Tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace WPForms\Tests;
 
+use Exception;
 use ReflectionClass;
 use PHP_CodeSniffer\Config;
 use PHP_CodeSniffer\Ruleset;
@@ -12,13 +13,24 @@ class TestCase extends \PHPUnit\Framework\TestCase {
 
 	protected function process( Sniff $className ) {
 
-		$class   = new ReflectionClass( $className );
+		$class     = new ReflectionClass( $className );
+		$localFile = WPFORMS_TESTED_FILES_PATH . str_replace( 'Sniff.php', '.php', str_replace( WPFORMS_SNIFFS_PATH, '', $class->getFileName() ) );
+
+		if ( ! file_exists( $localFile ) ) {
+			throw new Exception(
+				sprintf(
+					'The %s file doesn\'t exist',
+					$localFile
+				)
+			);
+		}
+
 		$config  = new Config();
 		$ruleset = new Ruleset( $config );
 		$ruleset->registerSniffs( [ $class->getFileName() ], [], [] );
 		$ruleset->populateTokenListeners();
 		$phpcsFile = new LocalFile(
-			WPFORMS_TESTED_FILES_PATH . str_replace( 'Sniff.php', '.php', str_replace( WPFORMS_SNIFFS_PATH, '', $class->getFileName() ) ),
+			$localFile,
 			$ruleset,
 			$config
 		);
