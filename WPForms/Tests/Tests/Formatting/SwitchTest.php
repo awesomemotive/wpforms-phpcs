f889a99e1dc9573e42aac8fa5b8c8f8d6fee1ea7
Maxym Denysenko <mdenisenko95@gmail.com>
2021-04-02 15:48:12 +0300

    Add error line sorting

--- WPForms/Tests/Tests/Formatting/SwitchTest.php
+++ WPForms/Tests/Tests/Formatting/SwitchTest.php
@@ -21,7 +21,7 @@ class SwitchTest extends TestCase {
 
 		$phpcsFile = $this->process( new SwitchSniff() );
 
-		$this->fileHasErrors( $phpcsFile, 'AddEmptyLineBefore', [ 59, 63, 68, 89, 81, 85 ] );
+		$this->fileHasErrors( $phpcsFile, 'AddEmptyLineBefore', [ 59, 63, 68, 81, 85, 89 ] );
 		$this->fileHasErrors( $phpcsFile, 'RemoveEmptyLineBefore', [ 61, 66, 70 ] );
 	}
 }
