a5561defe30942d573de111133bb74437296f039
kagg-design <info@kagg.eu>
2021-12-16 22:44:00 +0200

    Fix determining the fully qualified class name to validate hook name.
    This reverts commit caf08919728b0f9835acc01a6b29eee8c041c101.

--- WPForms/Tests/TestedFiles/PHP/ValidateHooks.php
+++ WPForms/Tests/TestedFiles/PHP/ValidateHooks.php
@@ -1,14 +1,24 @@
 <?php
 
-apply_filters( 'wpforms_tests_testedfiles_php_validatehooks', true );
-apply_filters( 'wpforms_tests_testedfiles_php_validatehooks_extra_detail', true );
-do_action( 'wpforms_tests_testedfiles_php_validatehooks', true );
-do_action( 'wpforms_tests_testedfiles_php_validatehooks_extra_detail', true );
+namespace WPForms\Tests\TestedFiles\PHP;
 
-apply_filters( 'tests_testedfiles_php_validatehooks', true );
-apply_filters( 'tests_testedfiles_php_validatehooks_extra_detail', true );
-do_action( 'tests_testedfiles_php_validatehooks', true );
-do_action( 'tests_testedfiles_php_validatehooks_extra_detail', true );
+class ValidateHooks {
 
-apply_filters( 'invalid_wpforms_tests_testedfiles_php_validatehooks', true );
-do_action( 'invalid_wpforms_tests_testedfiles_php_validatehooks', true );
+	public function useHooks() {
+
+		// Valid.
+		apply_filters( 'wpforms_tests_testedfiles_php_validatehooks', true );
+		apply_filters( 'wpforms_tests_testedfiles_php_validatehooks_extra_detail', true );
+		do_action( 'wpforms_tests_testedfiles_php_validatehooks', true );
+		do_action( 'wpforms_tests_testedfiles_php_validatehooks_extra_detail', true );
+
+		// Invalid.
+		apply_filters( 'tests_testedfiles_php_validatehooks', true );
+		apply_filters( 'tests_testedfiles_php_validatehooks_extra_detail', true );
+		do_action( 'tests_testedfiles_php_validatehooks', true );
+		do_action( 'tests_testedfiles_php_validatehooks_extra_detail', true );
+
+		apply_filters( 'invalid_wpforms_tests_testedfiles_php_validatehooks', true );
+		do_action( 'invalid_wpforms_tests_testedfiles_php_validatehooks', true );
+	}
+}
